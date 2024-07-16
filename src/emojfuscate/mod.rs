ff70cbe7823380ea7bfd2c090daed34a1bb84d37
Axel Ulmestig <axel.ulmestig@gmail.com>
2024-07-16 09:29:37 +0200

    tidy up tests a bit

--- src/emojfuscate/mod.rs
+++ src/emojfuscate/mod.rs
@@ -17,8 +17,8 @@ mod tests {
         let original_message = uuid!("67e55044-10b1-426f-9247-bb680e5fe0c8");
         // let original_message : Uuid = Uuid::new_v4();
         let emojified = original_message.clone().emojfuscate();
-        let roundtrip_message = emojified.demojfuscate();
-        assert_eq!(roundtrip_message, Ok(original_message));
+        let roundtrip_message = emojified.clone().demojfuscate();
+        assert_eq!(roundtrip_message, Ok(original_message), "emojfuscated version: {}", emojified);
     }
 
     #[test]
@@ -29,37 +29,28 @@ mod tests {
         assert_eq!(roundtrip_message, Ok(original_message), "emojfuscated version: {}", emojified);
     }
 
-    #[test]
-    fn emojfuscate_u8_vec() {
-        let original_message = vec![1u8, 2, 3];
-        let emojified = original_message.clone().emojfuscate();
-        println!("emojfuscated version: {}", emojified);
-        let roundtrip_message = emojified.clone().demojfuscate();
-        assert_eq!(roundtrip_message, Ok(original_message), "emojfuscated version: {}", emojified);
-    }
-
     proptest! {
         // "\\PC*" generating arbitrary strings composed of arbitrary non-control characters
         #[test]
         fn emojfuscate_string(original_message in "\\PC*") {
             // let original_message = "Karin är söt <3".to_string();
             let emojified = original_message.clone().emojfuscate();
-            let roundtrip_message = emojified.demojfuscate();
-            assert_eq!(roundtrip_message, Ok(original_message));
+            let roundtrip_message = emojified.clone().demojfuscate();
+            assert_eq!(roundtrip_message, Ok(original_message), "emojfuscated version: {}", emojified);
         }
 
         #[test]
         fn emojfuscate_u8(original_message : u8) {
             let emojified = original_message.clone().emojfuscate();
-            let roundtrip_message = emojified.demojfuscate();
-            assert_eq!(roundtrip_message, Ok(original_message));
+            let roundtrip_message = emojified.clone().demojfuscate();
+            assert_eq!(roundtrip_message, Ok(original_message), "emojfuscated version: {}", emojified);
         }
 
         #[test]
         fn emojfuscate_u16(original_message : u16) {
             let emojified = original_message.clone().emojfuscate();
-            let roundtrip_message = emojified.demojfuscate();
-            assert_eq!(roundtrip_message, Ok(original_message));
+            let roundtrip_message = emojified.clone().demojfuscate();
+            assert_eq!(roundtrip_message, Ok(original_message), "emojfuscated version: {}", emojified);
         }
 
         #[test]
@@ -73,6 +64,13 @@ mod tests {
         #[test]
         fn emojfuscate_string_triple(string1 in "\\PC*", string2 in "\\PC*", string3 in "\\PC*") {
             let original_message = (string1, string2, string3);
+            let emojified = original_message.clone().emojfuscate();
+            let roundtrip_message = emojified.clone().demojfuscate();
+            assert_eq!(roundtrip_message, Ok(original_message), "emojfuscated version: {}", emojified);
+        }
+
+        #[test]
+        fn emojfuscate_u8_vec(original_message : Vec<u8>) {
             let emojified = original_message.clone().emojfuscate();
             let roundtrip_message = emojified.clone().demojfuscate();
             assert_eq!(roundtrip_message, Ok(original_message), "emojfuscated version: {}", emojified);
